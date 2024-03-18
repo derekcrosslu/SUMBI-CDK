67ff6cbcd0d00abc00059ef0e7794d67c9be9686
Derek Cross <derekcrosslu@gmail.com>
2024-03-18 17:21:48 -0500

    prisma init

---
+++ test/sumbi-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SumbiCdk from '../lib/sumbi-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sumbi-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SumbiCdk.SumbiCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
